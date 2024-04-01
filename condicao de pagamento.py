66c8cc8179ca647bc012f77bcec6ecc09edf3e9d
ThomasLima9 <165452976+ThomasLima9@users.noreply.github.com>
2024-03-31 22:29:51 -0300

    Add files via upload

---
+++ condicao de pagamento.py
@@ -0,0 +1,29 @@
+from math import *
+from string import * 
+
+#Entrada de dados
+
+produto = float(input('Digite o preço do produto: '))
+condicao = int(input('Condição de pagamento (1/2/3/4): '))
+
+#Calculos + Saida de Dados
+
+while condicao <=0 or condicao >=5:
+    print('Condição de pagamento inválido, digite novamente: ')
+    int(input('Condição de pagamento (1/2/3/4): '))
+
+if condicao == 1:
+    print('Valor a pagar: ', produto - (produto*10) / (100))
+
+if condicao == 2:
+    print('Valor a pagar: ', produto - (produto*15) / (100))
+    
+if condicao == 3:
+    print('Valor a pagar: ', produto )
+
+if condicao == 4:
+    print('Valor a pagar: ', produto + (produto*10) / (100))
+
+#Condição com if's 1 a 4.
+#Fórmula da porcentagem.
+    
---
+++ ma~ças.py
@@ -0,0 +1,22 @@
+from math import *
+from string import *
+
+#Dados 
+
+macas = int(input('Digite a quantidade de Macas: '))
+
+#Saida de dados
+
+while macas <= 0:
+    print('Digite um numero valido cuzao: ')
+    int(input('Digite a quantidade de Macas: '))
+
+if macas < 12:
+    print('O valor da compra é: R$', macas*0.30)
+
+elif macas >= 12 and macas < 24:
+    print('O valor da compra é: R$', macas*0.25)
+    
+else:
+    print('O valor da compra é: R$', macas*0.20)
+    
