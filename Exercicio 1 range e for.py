9b2cbd9b79e16676af62052ee82eb5a1e3676a9e
ThomasLima9 <165452976+ThomasLima9@users.noreply.github.com>
2024-10-10 16:37:27 -0300

    Add files via upload

---
+++ Exercicio 1 range e for.py
@@ -0,0 +1,34 @@
+listaA = []
+for a in range(1,11):
+    listaA.append(a)
+
+print(listaA)
+
+listaB = []
+
+for b in range(10 , 0, -1):
+    listaB.append(b)
+
+print(listaB)
+
+listaC = []
+for c in range(10,31,5):
+    listaC.append(c)
+
+print(listaC)
+    
+listaD = []
+for d in range(-20 ,21 , 8):
+    listaD.append(d)
+print(listaD)
+
+listaE = []
+for e in range(10 , 5 , -2):
+    listaE.append(e)
+
+print(listaE)
+
+listaF = []
+for f in range(15 , -1 , -3):
+    listaF.append(f)
+print(listaF)
